b6f5c4a6fbb51cea3a66f7491e264d52eb5742c5
Julian Steiner <steinerj@stoina-arch-linux.localdomain>
2019-12-29 16:45:08 +0100

    add server.go first functionality

--- server.go
+++ server.go
@@ -1,6 +1,34 @@
 package rest
 
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	controller "github.com/Stoina/go-rest/controller"
+	repo "github.com/Stoina/go-rest/repository"
+)
+
 // Server exported
 // Server ...
 type Server struct {
+	URL  string
+	Port int
+}
+
+// Start exported
+// Start ....
+func (server *Server) Start(repo repo.Repository) {
+
+	log.Println("Server url: " + server.URL)
+	log.Println("Server port: " + strconv.Itoa(server.Port))
+
+	log.Println("Repository: " + repo.Name())
+
+	controller := controller.NewController(repo)
+
+	http.Handle("/"+server.URL+"/", controller)
+
+	portString := ":" + strconv.Itoa(server.Port)
+	log.Fatal(http.ListenAndServe(portString, nil))
 }

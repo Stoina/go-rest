42a394f8bcced538baef8cd685403a6336ef6d77
Julian Steiner <steinerj@stoina-arch-linux.localdomain>
2019-12-30 14:14:42 +0100

    Move Test Repo in new folder structure.

--- test/test.go
+++ test/test.go
@@ -8,6 +8,7 @@ import (
 	rest "github.com/Stoina/go-rest"
 	repo "github.com/Stoina/go-rest/repository"
 	sqlRepo "github.com/Stoina/go-rest/repository/sql"
+	testRepo "github.com/Stoina/go-rest/repository/test"
 )
 
 func main() {
@@ -22,8 +23,8 @@ func testRestRepository() {
 		Port: 8080,
 
 		Repositories: []repo.Repository{
-			repo.NewTestRepository("Test-One", "Test-One"),
-			repo.NewTestRepository("Test-Two", "Test-Two")}}
+			testRepo.NewTestRepository("Test-One", "Test-One"),
+			testRepo.NewTestRepository("Test-Two", "Test-Two")}}
 
 	restServer.Start()
 }

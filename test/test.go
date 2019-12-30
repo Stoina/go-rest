32122d037543738640b6f429fa70759e22025d74
Julian Steiner <steinerj@stoina-arch-linux.localdomain>
2019-12-30 14:12:18 +0100

    Add Test for SQL Repo.

--- test/test.go
+++ test/test.go
@@ -1,26 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
+	db "github.com/Stoina/go-database"
 	rest "github.com/Stoina/go-rest"
 	repo "github.com/Stoina/go-rest/repository"
+	sqlRepo "github.com/Stoina/go-rest/repository/sql"
 )
 
 func main() {
-	testRestRepository()
+	testSQLRepository()
 }
 
 func testRestRepository() {
 
-	log.Println("Test rest server")
+	log.Println("Testing rest server...")
 
 	restServer := rest.Server{
-		Port: 12345,
+		Port: 8080,
 
 		Repositories: []repo.Repository{
 			repo.NewTestRepository("Test-One", "Test-One"),
 			repo.NewTestRepository("Test-Two", "Test-Two")}}
+
+	restServer.Start()
+}
+
+func testSQLRepository() {
+	dbConn, err := db.OpenDBConnection("postgres", "localhost", 5432, "postgres", "steinerj", "postgres")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	restServer := rest.Server{
+		Port: 8080,
+
+		Repositories: []repo.Repository{
+			sqlRepo.NewSQLRepository("Golang-Test", "golangtest", dbConn, &sqlRepo.SQLRepositoryConfig{Statement: "select * from golangtest"})}}
 
 	restServer.Start()
 }

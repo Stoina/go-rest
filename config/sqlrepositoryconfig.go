b002741573d75ea325b852c4164a215841d10748
Steiner Julian <julian_steiner7@outlook.com>
2020-01-08 16:07:56 +0100

    Remove statement from sql repo config.

--- config/sqlrepositoryconfig.go
+++ config/sqlrepositoryconfig.go
@@ -6,7 +6,6 @@ type SQLRepositoryConfig struct {
 	Name string `json:"Name"`
 	URL string `json:"URL"`
 	DatabaseConfig DatabaseConfig `json:"DBSettings"`
-	Statement string
 }
 
 // DatabaseConfig exported
@@ -18,4 +17,3 @@ type DatabaseConfig struct {
 	Username string `json:"Username"`
 	Password string `json:"Password"`
 } 
-
